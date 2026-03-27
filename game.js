a6132088c1eaaa8a1ad7d3afe0dbaca6c861fba0
desaster201717 <35503495+desaster201717@users.noreply.github.com>
2026-03-27 11:14:20 +0100

    Update game.js

--- game.js
+++ game.js
@@ -71,7 +71,7 @@ class BananaJoe {
         this.playerH = this.playerElem.offsetHeight;
         
         // Dynamic step size (speed)
-        this.stepSize = Math.max(0.2, Math.min(this.containerW * 0.0006, 0.5));
+        this.stepSize = Math.max(0.3, Math.min(this.containerW * 0.0006, 0.8));
     }
 
     centerPlayer() {
